f3a28c28bb86a787f78a3564299544e313d41ce7
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-09-07 17:10:10 -0300

    getting values of operators.

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 var Result = window.document.getElementById('resultado');
 var Number = window.document.getElementsByClassName('number');
-var Op = window.document.getElementsByClassName('operador');
+var Operator = window.document.getElementsByClassName('operador');
 var Apagador = window.document.getElementById('apagador')
 var Equal = window.document.getElementById('equal');
 
+//alcançar todos os elementos(números) com a classe number
 for (var i = 0; i < Number.length; i++) {
     Number[i].addEventListener("click", numerar);
 }
 
-function numerar(event) {
-    var valueBtn = parseInt(event.target.getAttribute("data-valor"));
-    result(valueBtn);
+//alcançar todos os elementos(operadores) com a classe operador
+for (var i = 0; i < Operator.length; i++) {
+    Operator[i].addEventListener("click", Operar);
 }
 
-function result(valor1) {
-    Result.innerHTML += valor1;
+Equal.addEventListener('click', equar)
+
+function numerar(event) {
+    var valueBtn = parseInt(event.target.getAttribute("data-valor"));
+    Result.innerHTML += `${valueBtn}`;
 }
+
+function Operar(event) {
+    var valueOp = event.target.getAttribute("data-valor");
+    Result.innerHTML += ` ${valueOp} `;
+}
+
+function equar(){
+ 
+}
+
+
