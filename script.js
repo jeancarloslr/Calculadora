4ebf90aed2823c5edebf90d2daa0c4a515d8679b
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-10-11 17:57:30 -0300

    operations done;

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ var Number = window.document.getElementsByClassName('number');
 var Operator = window.document.getElementsByClassName('operador');
 var Apagador = window.document.getElementById('apagador')
 var Equal = window.document.getElementById('equal');
+var mensagem = window.document.getElementsByClassName('msg');
 
-var valueBtn = "";   
+var valueBtn = "";
 var valueOp = "";
+var firstValue = ""; // Armazena o primeiro valor para a operação
 
-//alcançar todos os elementos(números) com a classe number
 for (var i = 0; i < Number.length; i++) {
     Number[i].addEventListener("click", numerar);
 }
 
-//alcançar todos os elementos(operadores) com a classe operador
 for (var i = 0; i < Operator.length; i++) {
     Operator[i].addEventListener("click", Operar);
 }
 
-Equal.addEventListener('click', equar)
+Equal.addEventListener('click', equar);
 
 function numerar(event) {
     valueBtn += event.target.getAttribute("data-valor");
@@ -25,19 +25,32 @@ function numerar(event) {
 }
 
 function Operar(event) {
-    if(valueBtn !== ""){
+    if (valueBtn !== "") {
+        firstValue = valueBtn;
         valueOp = event.target.getAttribute("data-valor");
-        Result.innerHTML = valueBtn + " " + valueOp + " ";
-        valueBtn = ""; // Reseta o número atual.;
+        Result.innerHTML = firstValue + " " + valueOp + " ";
+        valueBtn = "";
     }
 }
-
-function equar(){
-   if(valueBtn !== "" && valueOp === '+'){
-    var result = parseInt(valueBtn) + parseInt(valueBtn);
-    Result.innerHTML = result.toString(); 
-    console.log(result);
-   }
+function equar() {
+    if (valueBtn !== "" && firstValue !== "" && valueOp) {
+        var result;       
+        switch(valueOp){
+            case '+':
+                result = parseFloat(firstValue) + parseFloat(valueBtn);
+                break;
+            case '-':
+                result = parseFloat(firstValue) - parseFloat(valueBtn);
+                break;
+            case 'X':
+                result = parseFloat(firstValue) * parseFloat(valueBtn);
+                break;
+            case '/':
+                result = parseFloat(firstValue) / parseFloat(valueBtn);
+                break
+            default:
+                mensagem.innerHTML = 'Operador desconhecido';
+        }      
+        Result.innerHTML = result.toString();
+    }
 }
-
-
